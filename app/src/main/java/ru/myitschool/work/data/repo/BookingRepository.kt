763a7cfc10e84e62b28d6205f32c38b86ee7d594
tomas <chanyshevd6@gmail.com>
2025-12-05 23:02:56 +0700

    feat(client) Requests Done

--- app/src/main/java/ru/myitschool/work/data/repo/BookingRepository.kt
+++ app/src/main/java/ru/myitschool/work/data/repo/BookingRepository.kt
@@ -3,7 +3,6 @@ package ru.myitschool.work.data.repo
 import ru.myitschool.work.data.dto.BookingResponse
 import ru.myitschool.work.data.dto.UserInfoResponse
 import ru.myitschool.work.data.source.NetworkDataSource
-import ru.myitschool.work.domain.model.BookingsResponse
 
 object BookingRepository {
 
